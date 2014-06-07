b7b2f593fa0388a2f15f835e258b7678c2af1012
Diego Ponce <diegoponce@outlook.com>
2014-06-07 17:57:24 -0500

    Add all files

---
+++ index.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+
+   $(function() { //When the document loads
+      $("#agenda").bind("click", function() {
+        $(window).scrollTop($(".agenda").offset().top); //scroll to div with container as ID.
+        return false; //Prevent Default and event bubbling.
+      
+      });
+    }); 
+
+   
+
+   $(function() { //When the document loads
+      $("#mentores").bind("click", function() {
+        $(window).scrollTop($(".mentores").offset().top), 2000; //scroll to div with container as ID.
+        return false; //Prevent Default and event bubbling.
+      });
+    });
+
+   $(function() { //When the document loads
+      $("#jueces").bind("click", function() {
+        $(window).scrollTop($(".jueces").offset().top); //scroll to div with container as ID.
+        return false; //Prevent Default and event bubbling.
+      });
+    });
+
+   $(function() { //When the document loads
+      $(".sede").bind("click", function() {
+        $(window).scrollTop($(".lugar").offset().top); //scroll to div with container as ID.
+        return false; //Prevent Default and event bubbling.
+      });
+    });
+
+    $(function() { //When the document loads
+      $(".perfiles").bind("click", function() {
+        $(window).scrollTop($(".tickets").offset().top); //scroll to div with container as ID.
+        return false; //Prevent Default and event bubbling.
+      });
+    });
+
+     $(function() { //When the document loads
+      $("#patrocinadores").bind("click", function() {
+        $(window).scrollTop($(".patrocinadores").offset().top), 2000; //scroll to div with container as ID.
+        return false; //Prevent Default and event bubbling.
+      });
+    });
+
+
+});
+
+
